efdb0e7db68c854813e03157c4983f39f475ca89
arozycka <agnieszka.rozycka2018@gmail.com>
2020-03-21 15:58:43 +0100

    Updates

---
+++ precache-manifest.b927d8a8e3dc9ec1bd79b3f15208031f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906551c8cabaaf5e259e447678f1be42",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "4ecb02994616fdf889a6",
+    "url": "/flashcards/static/css/main.5685fb23.chunk.css"
+  },
+  {
+    "revision": "8cba29e564d0bae81a94",
+    "url": "/flashcards/static/js/2.f06260a6.chunk.js"
+  },
+  {
+    "revision": "da179cc08d2ec2998ceaf40e172262b3",
+    "url": "/flashcards/static/js/2.f06260a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ecb02994616fdf889a6",
+    "url": "/flashcards/static/js/main.20982ed4.chunk.js"
+  },
+  {
+    "revision": "090e9a9e8463754c061d",
+    "url": "/flashcards/static/js/runtime-main.8656b6d5.js"
+  }
+]);
